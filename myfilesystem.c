914e201f4c7a2d7e2f03b6e994d0273f8314dfc6
Oscar Gao <oscargao@oscars-air.home>
2019-05-06 21:39:44 +1000

    Again

--- myfilesystem.c
+++ myfilesystem.c
@@ -5,14 +5,15 @@
 
 #include <pthread.h>
 
-int threads;
-char *file1;
-char *file2;
-char *file3;
-// these files are fixed in size. they will not run out of space
-FILE *file_data;
-FILE *directory_table;
-FILE *hash_data;
+struct HelpStruct{
+   // these files are fixed in size. they will not run out of space
+   FILE *file_data;
+   FILE *directory_table;
+   FILE *hash_data;
+   int dir_size;
+   int threads_max;
+   int threads;
+}Help;
 
 // this is the first function run. initialise all data structures needed.
 // Return a pointer to a memory area (void *) that you can use to store data for
@@ -25,57 +26,84 @@ FILE *hash_data;
 // Set up threads. I guess I can only go up to n_processors.
 
 void * init_fs(char * f1, char * f2, char * f3, int n_processors) {
-   void* space = (void*) malloc(100);
-   file1 = f1;
-   file2 = f2;
-   file3 = f3;
    //set up the files. If they don't exist, it will be created
-   file_data = fopen(f1,"rb+");
-   directory_table = fopen(f2,"rb+");
-   hash_data = fopen(f3, "rb+");
-   // create threads
-   threads = n_processors;
-   return space;
+
+
+   Help * helper = (Help*) malloc(sizeof(Help));
+   helper->file_data = fopen(f1,"rb+");
+   helper->directory_table = fopen(f2,"rb+");
+   helper->hash_data = fopen(f3, "rb+");
+   FILE* file1 = helper->file_data;
+   FILE* file2 = helper->directory_table;
+   FILE* file3 = helper->hash_data;
+   fseek(file2,0,SEEK_END);
+   int len = ftell(file2);
+   helper->dir_size = len;
+   fseek(file2,0,SEEK_SET);
+   helper->thread_max = n_processors;
+   helper->threads = 0;
+   return helper;
 }
 
 void close_fs(void * helper) {
-   fclose(file_data);
-   fclose(directory_table);
-   fclose(hash_data);
+   fclose(helper->file_data);
+   fclose(helper->directory_table);
+   fclose(helper->hash_data);
    free(helper);
 }
 
-int create_file(char * filename, size_t length, void * helper) {
-   fseek(directory_table,0,SEEK_END);
-   int len = ftell(directory_table);
-   fseek(directory_table,0,SEEK_SET);
-   char buf[len];
-   fread(buf,sizeof(buf),1,directory_table); // buf should now contain every bye of the directory_table file
 
-   int l = strlen(filename);
-   int progress = 0;
-   // put this in one thread
-   // Checking if filename exists
-   for(int i = 0; i < sizeof(buf);i++){
-      if(progress<l){
-         if(buf[i]==filename[progress]){
-            progress++;
-         }else{
-            progress=0;
-         }
+void truncate(char* filename){
+   char truncated[64];
+   truncated[63]='\0';
+   for(int i = 0; i<63; i++){
+      if(strlen(filename)>i){
+         truncated[i]=filename[i];
+      }else{
+         truncated[i]='\0';
       }
-      //at the end of this, if the whole thing is reached, progress should be l
-      if(progress==l){
+   }
+   return truncated;
+}
+
+int create_file(char * filename, size_t length, void * helper) {
+
+   //int l = strlen(filename);
+   char * buf[helper->dir_size];
+   fread(buf,sizeof(char),helper->dir_size,helper->directory_table);
+
+   //check if the filename already exists
+   for(int i = 0; i< helper->dir_size;i+=72){
+      char* truncated = truncate(filename);
+      if(strncmp(buf[i],truncated,64)==0){
          return 1;
       }
    }
-   // putting this in the current thread
-   // Checking for empty spaces
-   for(int i = 0; i < sizeof(buf); i++){
-      if(progress<l){
-         if(buf[i]==NULL){
-            s
-         }
+   //check an empty space
+   int success = -1;
+   for(int i = 0; i< helper->dir_size; i+=72){
+      if(i==0){
+         fseek(helper->directory_table,0,SEEK_SET);
+      }else{
+         fseek(helper->directory_table,72,SEEK_CUR);
+      }
+      char *test;
+      test = fgets(test, 1,helper->directory_table);
+      if(*test=='\0'){
+         success = i;
+      }
+   }
+   fseek(helper->directory_table,0,SEEK_SET);
+   if(success !=-1){
+      // just do it
+      for(int i = 0; i< 72; i++){
+         buf[72*success+i] = truncated[i];
+      }
+   }else{
+      // repack then do it.
+      if(){
+         // if it fails, then return 1;
+
       }
    }
 
--- test.c
+++ test.c
@@ -3,7 +3,20 @@
 
 int main(int argc, char** argv){
    char * test = "123";
-   print("%c,%c,%c\n"test[0],test[1],test[2]);
+   printf("%c,%c,%c,%c,%c\n",test[0],test[1],test[2],test[3],test[4]);
+   if(test[3]=='\0'){
+      printf("test\n");
+   }
+   if(test[4]=='\0'){
+      printf("test2\n");
+   }
+   char * abc = "123\0";
+   if(abc[3]=='\0'){
+      printf("|||||prev\n");
+   }
+   if(abc[4]=='\0'){
+      printf("|||||\n");
+   }
 
 
    return 0;
